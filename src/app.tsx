350d602ba815cd34f758dfce8a5ec05ac2221fd3
ismaelp96 <ismael6991@live.com>
2024-02-22 17:09:57 -0300

    att

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 
 import { Control, Input } from './components/ui/input';
 import { Button } from './components/ui/button';
@@ -14,7 +16,46 @@ import {
 } from './components/ui/table';
 import { Pagination } from './components/pagination';
 
+export interface TagResponse {
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
+}
+
+export interface Tag {
+  title: string;
+  amountOfVideos: number;
+  id: string;
+}
+
 export function App() {
+  const [searchParams] = useSearchParams();
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1;
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', page],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3333/tags?_page=${page}&_per_page=10`
+      );
+      const data = await response.json();
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      return data;
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className='py-10 space-y-8'>
       <div>
@@ -49,17 +90,19 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className='flex flex-col gap-0.5'>
-                      <span className='font-medium'>React</span>
-                      <span className='text-xs text-zinc-500'>ID</span>
+                      <span className='font-medium'>{tag.title}</span>
+                      <span className='text-xs text-zinc-500'>{tag.id}</span>
                     </div>
                   </TableCell>
-                  <TableCell className='text-zinc-300'>13 videos(s)</TableCell>
+                  <TableCell className='text-zinc-300'>
+                    {tag.amountOfVideos} videos(s)
+                  </TableCell>
                   <TableCell className='text-right'>
                     <Button size='icon'>
                       <MoreHorizontal className='size-4' />
@@ -70,7 +113,13 @@ export function App() {
             })}
           </TableBody>
         </Table>
-        <Pagination />
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={page}
+          />
+        )}
       </main>
     </div>
   );
